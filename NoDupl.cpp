a8a2b3c6d5ffc9fab3d0664e76b9b51a4b894c5b
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-14 20:45:10 +0200

    Create NoDupl.cpp

---
+++ NoDupl.cpp
@@ -0,0 +1,50 @@
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <map>
+
+
+int main()
+{
+	bool duplicate{ false };
+	std::string input{ "" };
+	// If we used std:: cin >> then we would only get the first word as cin cuts off when space is encountered.
+	std::getline(std::cin, input); 
+
+	std::vector<std::string> v;
+	std::stringstream ss(input);
+	std::string word;
+
+	while (ss >> word)
+	{
+		v.push_back(word);
+	}
+
+	std::map<std::string, int> countMap;
+	for (auto & element : v)
+	{
+		auto result = countMap.insert(std::pair<std::string, int> (element, 1));
+		if (!result.second)
+		{
+			result.first->second++;
+		}
+	}
+	for (auto & element : countMap)
+	{
+		if (element.second > 1)
+		{
+			duplicate = true;
+		}
+	}
+
+	if (duplicate)
+	{
+		std::cout << "no" << std::endl;
+	}
+	else {
+		std::cout << "yes" << std::endl;
+	}
+
+	return 0;
+}
+
