a30b2c97702082fd7837005ad451b13674c17ea6
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-15 19:49:57 +0200

    Update speedlimit.cpp

--- speedlimit.cpp
+++ speedlimit.cpp
@@ -5,22 +5,22 @@
 using namespace std;
 int main()
 {
-    int input = 0, mile_a = 0, mile_b = 0;
+	int input{ 0 }, mile_a{ 0 }, mile_b{ 0 };
 
-    while (input != -1) {
-    int result = 0, prevMile = 0;
+	while (input != -1) {
+		int result{ 0 }, prevMile{ 0 };
 
-      cin >> input;
-    
-      for (int i = 0; i < input; i++)
-      {
-          cin >> mile_a;
-          cin >> mile_b;
-          result = result + ((mile_a) * (mile_b - prevMile));
-          prevMile = mile_b;
-      }
-      if(input != -1)
-          cout << result << " miles" << endl;
-    }
-    return 0;
+		cin >> input;
+	
+		for (int i = 0; i < input; i++)
+		{
+			cin >> mile_a;
+			cin >> mile_b;
+			result = result + ((mile_a) * (mile_b - prevMile));
+			prevMile = mile_b;
+		}
+		if(input != -1)
+			cout << result << " miles" << endl;
+	}
+	return 0;
 }
