946fd884db9836acc6a94393d9982ac8886fda44
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-18 19:17:48 +0200

    Create cold.cpp

---
+++ cold.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int amount_of_temperatures, temp, counter{ 0 };
+
+    std::cin >> amount_of_temperatures;
+
+    for (int i = 0; i < amount_of_temperatures; i++)
+    {
+        std::cin >> temp;
+
+        if (temp < 0)
+        {
+            counter++;
+        }
+    }
+    std::cout << counter << std::endl;
+    return 0;
+}
