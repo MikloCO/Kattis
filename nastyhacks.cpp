cf5abfe40b349eea94eea39f82b1691de1ed567e
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-18 19:19:06 +0200

    Create nastyhacks.cpp

---
+++ nastyhacks.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    int num_cases = 0;
+    int revenue, expected_revenue, cost_advertisement, after_advertisement{0};
+    std::cin >> num_cases;
+    std::string answer;
+
+    for (int i = 0; i < num_cases; i++) {
+        std::cin >> revenue >> expected_revenue >> cost_advertisement;
+        after_advertisement = cost_advertisement + revenue;
+
+            if (expected_revenue == after_advertisement)
+            {
+                answer += "does not matter\n";
+            }
+            else if (after_advertisement < expected_revenue)
+            {
+                answer += "advertise\n";
+            }
+            else 
+            {
+                answer += "do not advertise\n";
+            }
+    }
+
+    std::cout << answer;
+
+    return 0;
+}
