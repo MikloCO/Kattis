3cc0f5a6d13f577c76af89c10e3192a0a551ebed
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-18 19:13:43 +0200

    Create simonsays.cpp

---
+++ simonsays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	int test_cases{ 0 };
+	std::string words, output;
+
+	cin >> test_cases;
+
+	for (int i = 0; i <= test_cases; i++)
+	{
+		getline(cin, words);
+		if (words.substr(0,10).compare("Simon says") == 0)
+		{
+			cout << words.substr(11, words.length()) << endl;
+		}
+	}
+
+    return 0;
+}
+
