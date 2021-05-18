4a61a5c710723299ad67060e173682e6355b7d66
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-18 19:20:06 +0200

    Create quadrant.cpp

---
+++ quadrant.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    int coordX, coordY;
+    
+    std::cin >> coordX >> coordY;
+
+    if (coordX >= 0 && coordY >= 0)
+    {
+        std::cout << 1 << std::endl;
+    }
+    else if (coordX <= 0 && coordY <= 0) 
+    {
+        std::cout << 3 << std::endl;
+    }
+    else if (coordX >= 0 && coordY <= 0)
+    {
+        std::cout << 4 << std::endl;
+    }
+    else {
+        std::cout << 2 << std::endl;
+    }
+
+    return 0;
+}
