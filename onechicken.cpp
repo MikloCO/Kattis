bd3a2c18d0d3f0c98984b282df859c12e2eaef2d
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-18 19:15:56 +0200

    Create onechicken.cpp

---
+++ onechicken.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+    int piece_a, piece_b, rest{ 0 };
+    std::cin >> piece_a >> piece_b;
+
+
+    if (piece_a < piece_b)
+    {
+        rest = piece_b - piece_a;
+        if (rest == 1)
+        {
+            std::cout << "Dr. Chaz will have " << rest <<
+                " piece of chicken left over!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Dr. Chaz will have " << rest <<
+                " pieces of chicken left over!" << std::endl;
+        }
+    }
+    else
+    {
+        rest = piece_a - piece_b;
+        if (rest == 1)
+        {
+            std::cout << "Dr. Chaz needs " << rest <<
+                " more piece of chicken!" << std::endl;
+        }
+        else 
+        {
+
+        std::cout << "Dr. Chaz needs " << rest <<
+            " more pieces of chicken!" << std::endl;
+        }
+    }
+}
