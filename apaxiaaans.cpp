b2df080daed6faee49d7cfd089b4d839a7c9e07e
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-22 19:05:33 +0200

    Update apaxiaaans.cpp

--- apaxiaaans.cpp
+++ apaxiaaans.cpp
@@ -1,19 +1,13 @@
-//https://open.kattis.com/problems/apaxiaaans
 #include <iostream>
 #include <string>
 
-int main()
-{
+int main() {
     std::string input;
     std::cin >> input;
-
-    for (int i = 0; i < input.length(); i++)
-    {
+    for (int i = 0; i < input.length(); i++) {
         if (input[i] != input[i + 1])
             std::cout << input[i];
     }
-    
     std::cout << std::endl;
-    
     return 0;
-}
+} //https://open.kattis.com/problems/apaxiaaans
