87c29eca45e9f1a607be8470bb7df630cc1a595a
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-27 18:58:20 +0200

    Update oddmanout.cpp

--- oddmanout.cpp
+++ oddmanout.cpp
@@ -1,32 +1,31 @@
 //https://open.kattis.com/problems/oddmanout
 #include <iostream>
-#include <algorithm>
 #include <unordered_map>
 
 using namespace std;
 
 int main()
 {
-	int total_test_cases, case_N, guest_no, num = 1;
-	cin >> total_test_cases;
+    int total_test_cases, case_N, guest_no, num = 1;
+    cin >> total_test_cases;
 
-	for (int i = 0; i < total_test_cases; i++)
-	{
-		unordered_map<int, int> guestlist;
-		cin >> case_N;
-		
-		for (int j = 0; j < case_N; j++)
-		{
-			cin >> guest_no;
-			guestlist[guest_no]++;
-		}
-		
-		for (auto guest : guestlist) {
-			if (guest.second == 1) {
-				cout << "Case #" << num++ << ": " << guest.first << endl;
-			}
-		}
-	}
+    for (int i = 0; i < total_test_cases; i++)
+    {
+        unordered_map<int, int> guestlist;
+        cin >> case_N;
+        
+        for (int j = 0; j < case_N; j++)
+        {
+            cin >> guest_no;
+            guestlist[guest_no]++;
+        }
+        
+        for (auto guest : guestlist) {
+            if (guest.second == 1) {
+                cout << "Case #" << num++ << ": " << guest.first << endl;
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
