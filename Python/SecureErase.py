c6ff2d3056aab884e360abfb275d66255cb28a40
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-09-23 22:35:18 +0200

    Update SecureErase.py

--- Python/SecureErase.py
+++ Python/SecureErase.py
@@ -2,14 +2,13 @@
 deletion_amount = int(input())
 file_1 = input()
 file_2 = input()
-file_1_length = str(file_1)
 counter = 0
 
 for i in range(len(file_1)):
     if bool(int((file_1[i]))) ^ bool(int((file_2[i]))):
         counter += 1
 
-if (counter == len(file_1_length)) or (deletion_amount % 2 == 0 and file_1 == file_2):
+if (counter == len(file_1)) or (deletion_amount % 2 == 0 and file_1 == file_2):
     print("Deletion succeeded")
 else:
     print("Deletion failed")
